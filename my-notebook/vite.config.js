56fde4c2c8bd4c251be5c27d8a02bd42ca3a187e
泡泡 <lucinda950724@gmail.com>
2026-04-17 12:37:20 +0800

    更新說明（例：新增篩選功能）

--- my-notebook/vite.config.js
+++ my-notebook/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base: '/my-notebook/',   //
+  base: '/20260417/'   // ⭐ 改成你的 repo 名
 })
